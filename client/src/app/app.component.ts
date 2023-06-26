7e40393cfd28896460000182b267442510edb87d
CHI-KEKE <chilly37647@gmail.com>
2023-06-26 23:35:55 +0800

    angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component,OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +9,10 @@ import { HttpClient } from '@angular/common/http';
 
 export class AppComponent implements OnInit{
   title = 'Skinet';
-  products:Product[] = [];
 
-  constructor(private http:HttpClient){}
+  constructor(){}
 
     ngOnInit(): void{
-      this.http.get<Pagination<Product[]>>("https://localhost:5001/api/products?pageSize=50").subscribe({
-        next: response => this.products = response.data,
-        error:error => console.log(error),
-        complete:() => {console.log("request completed");
-                        console.log("extra statement");}
-      })
-    
+
   }
 }
